907630270ed3e1fe4a83f2c8625c1de4b1b02dd4
Agência H1 Code <contato@h1code.com.br>
2018-11-06 19:02:33 -0200

    Identação do código

--- Scripts/GerenciaFirewall.sh
+++ Scripts/GerenciaFirewall.sh
@@ -9,14 +9,14 @@ PATH=/sbin:/usr/sbin:/usr/bin:/bin
 # Variáveis Globais
 
 consulta=/etc/sysconfig/iptables
-salvar=$( service iptables save > /dev/null )
+salvar=$(service iptables save >/dev/null)
 
 # Variáveis de Cores
 
-verde="\e[00;32m"		# Cor Verde: Entrada
-vermelho="\e[00;31m"	# Cor Vermelho: Entrada
-amarelo_e="\e[00;33m"	# Cor Amarelho: Entrada
-cor_s="\e[00m"			# Saida de Cor: Original SHELL
+verde="\e[00;32m"     # Cor Verde: Entrada
+vermelho="\e[00;31m"  # Cor Vermelho: Entrada
+amarelo_e="\e[00;33m" # Cor Amarelho: Entrada
+cor_s="\e[00m"        # Saida de Cor: Original SHELL
 
 # Menu de Opções
 
@@ -36,101 +36,96 @@ echo ""
 # Funções das opções escolhidas
 
 case $opcao in
-		1)
-			echo ""
-			echo -n " >>> DIGITE A PORTA QUE DESEJA LIBERAR: "
-			read liberar_porta
-			echo ""
-			
-			cat $consulta | grep "dport $liberar_porta"
-			
-			if [ $? = 1 ]
-			then
-				iptables -A INPUT -p tcp --dport $liberar_porta -j ACCEPT
-				iptables -A INPUT -p udp --dport $liberar_porta -j ACCEPT
-				$salvar
-				echo ""
-				echo -e "$verde PORTA $liberar_porta LIBERADA COM SUCESSO NO FIREWALL! $cor_s"
-				echo ""
-			else
-				echo ""
-				echo -e "$vermelho A PORTA $liberar_porta JÁ SE ENCONTRA LIBERADA NO FIREWALL, NADA À FAZER! $cor_s"
-				echo ""
-			fi
-		;;
-		2)
-			echo ""
-			echo -n " >>> DIGITE A PORTA QUE DESEJA BLOQUEAR: "
-			read bloquear_porta
-			echo ""
-			
-			iptables -D INPUT -p tcp --dport $bloquear_porta -j ACCEPT
-			
-			while [ $? = 0 ]
-			do
-				iptables -D INPUT -p tcp --dport $bloquear_porta -j ACCEPT
-			done
-			
-			iptables -D INPUT -p udp --dport $bloquear_porta -j ACCEPT
-		
-			while [ $? = 0 ]
-			do
-				iptables -D INPUT -p udp --dport $bloquear_porta -j ACCEPT
-			done
-			$salvar
-			echo ""
-			echo -e "$verde A PORTA $bloquear_porta FOI BLOQUEADA COM SUCESSO! $cor_s"
-			echo ""
-		;;
-		3)
-			echo ""
-			echo -n " >>> DIGITE O IP QUE DESEJA LIBERAR: "
-			read liberar_ip
-			echo ""
-			
-			cat $consulta | grep $liberar_ip
-			
-			if [ $? = 1 ]
-			then
-				iptables -A INPUT -s $liberar_ip -j ACCEPT
-				$salvar
-				echo ""
-				echo -e "$verde O IP $liberar_ip FOI LIBERADO COM SUCESSO NO FIREWALL $cor_s"
-				echo ""
-			else
-				echo ""
-				echo -e "$vermelho O IP JÁ SE ENCONTRA LIBERADO NO FIREWALL, NADA À FAZER! $cor_s"
-				echo ""
-			fi
-		;;	
-		4)
-			echo ""
-			echo -n " >>> DIGITE O IP QUE DESEJA BLOQUEAR: "
-			read bloquear_ip
-			echo ""
-			
-			iptables -D INPUT -s $bloquear_ip -j ACCEPT
-			
-			while [ $? = 0 ]
-			do
-				iptables -D INPUT -s $bloquear_ip -j ACCEPT
-			done
-			$salvar
-			echo ""
-			echo -e "$verde O IP FOI REMOVIDO COM SUCESSO DO FIREWALL! $cor_s"
-			echo ""
-		;;	
-		5)
-			echo ""
-			echo -n " >>> DIGITE A NOVA REGRA PADRÃO (ACCEPT OU DROP): "
-			read regra_padrao
-			echo ""
-			
-			iptables -P INPUT $regra_padrao
-			$salvar
-			echo ""
-			echo -e "$verde A REGRA PADRÃO FOI ALTERADA COM SUCESSO PARA: $regra_padrao $cor_s"			
-			echo ""
-		;;	
-						
-esac		
+1)
+	echo ""
+	echo -n " >>> DIGITE A PORTA QUE DESEJA LIBERAR: "
+	read liberar_porta
+	echo ""
+
+	cat $consulta | grep "dport $liberar_porta"
+
+	if [ $? = 1 ]; then
+		iptables -A INPUT -p tcp --dport $liberar_porta -j ACCEPT
+		iptables -A INPUT -p udp --dport $liberar_porta -j ACCEPT
+		$salvar
+		echo ""
+		echo -e "$verde PORTA $liberar_porta LIBERADA COM SUCESSO NO FIREWALL! $cor_s"
+		echo ""
+	else
+		echo ""
+		echo -e "$vermelho A PORTA $liberar_porta JÁ SE ENCONTRA LIBERADA NO FIREWALL, NADA À FAZER! $cor_s"
+		echo ""
+	fi
+	;;
+2)
+	echo ""
+	echo -n " >>> DIGITE A PORTA QUE DESEJA BLOQUEAR: "
+	read bloquear_porta
+	echo ""
+
+	iptables -D INPUT -p tcp --dport $bloquear_porta -j ACCEPT
+
+	while [ $? = 0 ]; do
+		iptables -D INPUT -p tcp --dport $bloquear_porta -j ACCEPT
+	done
+
+	iptables -D INPUT -p udp --dport $bloquear_porta -j ACCEPT
+
+	while [ $? = 0 ]; do
+		iptables -D INPUT -p udp --dport $bloquear_porta -j ACCEPT
+	done
+	$salvar
+	echo ""
+	echo -e "$verde A PORTA $bloquear_porta FOI BLOQUEADA COM SUCESSO! $cor_s"
+	echo ""
+	;;
+3)
+	echo ""
+	echo -n " >>> DIGITE O IP QUE DESEJA LIBERAR: "
+	read liberar_ip
+	echo ""
+
+	cat $consulta | grep $liberar_ip
+
+	if [ $? = 1 ]; then
+		iptables -A INPUT -s $liberar_ip -j ACCEPT
+		$salvar
+		echo ""
+		echo -e "$verde O IP $liberar_ip FOI LIBERADO COM SUCESSO NO FIREWALL $cor_s"
+		echo ""
+	else
+		echo ""
+		echo -e "$vermelho O IP JÁ SE ENCONTRA LIBERADO NO FIREWALL, NADA À FAZER! $cor_s"
+		echo ""
+	fi
+	;;
+4)
+	echo ""
+	echo -n " >>> DIGITE O IP QUE DESEJA BLOQUEAR: "
+	read bloquear_ip
+	echo ""
+
+	iptables -D INPUT -s $bloquear_ip -j ACCEPT
+
+	while [ $? = 0 ]; do
+		iptables -D INPUT -s $bloquear_ip -j ACCEPT
+	done
+	$salvar
+	echo ""
+	echo -e "$verde O IP FOI REMOVIDO COM SUCESSO DO FIREWALL! $cor_s"
+	echo ""
+	;;
+5)
+	echo ""
+	echo -n " >>> DIGITE A NOVA REGRA PADRÃO (ACCEPT OU DROP): "
+	read regra_padrao
+	echo ""
+
+	iptables -P INPUT $regra_padrao
+	$salvar
+	echo ""
+	echo -e "$verde A REGRA PADRÃO FOI ALTERADA COM SUCESSO PARA: $regra_padrao $cor_s"
+	echo ""
+	;;
+
+esac
