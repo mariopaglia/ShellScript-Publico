78deb27621bcfa417226d9ef5d10321c0ca4f1f9
Agência H1 Code <mario@h1code.com.br>
2019-11-20 20:28:54 -0300

    Adição de 2 novos scripts

---
+++ Scripts/GerenciarServidorWeb.sh
@@ -0,0 +1,54 @@
+﻿#!/bin/bash
+
+VERIFICA=$(ps aux | grep -v "grep" | grep -E "apache2|mysql" | wc -l);
+
+echo
+echo "########## O QUE DESEJA FAZER? ##########"
+echo
+echo "1) Ligar Servidor Web"
+echo "2) Reiniciar Servidor Web"
+echo "3) Desligar Servidor Web"
+echo
+
+read -p "O que deseja fazer? " OPCAO
+
+case $OPCAO in
+    1)
+        if [ $VERIFICA -ge "2" ]
+        then
+            echo "O Servidor Web já estava ligado!";
+        else
+            sudo service apache2 start;
+            sudo service mysql start;
+            echo "Servidor Web ligado com sucesso!";
+        fi
+    ;;
+
+    2)
+        if [ $VERIFICA -ge "2" ]
+        then
+            sudo service apache2 restart;
+            sudo service mysql restart;
+            echo "Servidor Web reiniciado com sucesso!";
+        else
+            echo "Houve algum problema ao reiniciar o Servidor Web";
+        fi
+    ;;
+
+    3)
+        if [ $VERIFICA -ge "2" ]
+        then
+            sudo service apache2 stop;
+            sudo service mysql stop;
+            echo "Servidor Web desligado com sucesso!";
+        else
+            echo "O Servidor Web já estava desligado!";
+        fi
+    ;;
+
+    *)
+        echo "Informe uma opção válida!";
+    ;;
+esac
+
+$SHELL
---
+++ Scripts/PosFormatacaoLinuxMint.sh
@@ -0,0 +1,52 @@
+﻿#!/bin/bash
+
+# Desbloqueia o APT-GET
+
+sudo killall apt apt-get;
+
+# Update no apt-get
+
+sudo apt-get update;
+sudo apt upgrade -y;
+
+# Instalar programas essenciais
+
+sudo apt-get install vim whatsapp-desktop htop wine-installer vlc git filezilla thunderbird putty mint-meta-codecs steam:i386 audacity spotify-client gparted snapd -y
+
+# Instalar programas via Flatpak
+
+sudo flatpak install flathub org.telegram.desktop com.discordapp.Discord com.obsproject.Studio com.visualstudio.code -y;
+
+# Verificar se tem placa NVIDIA e instalar dependencia
+
+if lspci | grep "NVIDIA" > /dev/null
+then
+	sudo apt-get install nvidia-settings;
+fi
+
+# Mudar para pasta onde será baixado os .deb
+
+mkdir /home/$USER/Documentos/Programas;
+
+cd /home/$USER/Documentos/Programas;
+
+# Instalar o Google Chrome e OpenDrive (Google Drive)
+
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sugo wget https://github.com/liberodark/ODrive/releases/download/0.3.0/odrive_0.3.0_amd64.deb
+
+sudo dpkg -i *.deb;
+
+
+# Instalar servidor web
+
+# sudo apt install -y apache2 php php-cli php-common php-gd php-mbstring php-intl php-xml php-zip php-pear libapache2-mod-php mysql-server mysql-client php-mysql phpmyadmin;
+
+sudo apt-get install lamp-server^ -y;
+
+# Instalar XenCenter
+
+sudo apt-get install python-gtk2 glade python-gtk-vnc python-glade2 python-configobj python-setuptools -y;
+git clone https://github.com/OpenXenManager/openxenmanager.git;
+cd openxenmanager;
+sudo python setup.py install;
